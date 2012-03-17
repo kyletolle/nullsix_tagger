fe6eb0ee2398f690bb88d406e581d58e8ef6f7d4
Kyle Tolle <kyle@nullsix.com>
2012-03-16 20:52:57 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NullsixTagger::Application.config.secret_token = 'ae7b9a3a6c1af4fff871108f3186da1ec3046ee4e7266c624d809ef46cb169d46b353e96b019472153d3dc59d40a6946d34aac1a37c6f57e679f224724c999e9'
