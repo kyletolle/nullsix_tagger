fdd5c921ebb81a226a9a280bc8a300b41d58adf5
Kyle Tolle <kyle@nullsix.com>
2012-03-19 23:45:48 -0600

    Deleting link redirects to root.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -79,7 +79,7 @@ class LinksController < ApplicationController
     @link.destroy
 
     respond_to do |format|
-      format.html { redirect_to links_url }
+      format.html { redirect_to "/" }
       format.json { head :no_content }
     end
   end
