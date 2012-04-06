5c69e4dc334a66f7f9be0d85ea1a19c0ec990409
Kyle Tolle <kyle@nullsix.com>
2012-04-05 21:05:30 -0600

    Removed Test::Unit as the test framework

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120405041908) do
+ActiveRecord::Schema.define(:version => 20120406023345) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20120405041908) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
 
   create_table "links", :force => true do |t|
     t.string   "url"
