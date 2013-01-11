0625b9b3cd188ad88ca4f46dfd8e1fb54325dd8a
Kyle Tolle <kyle@nullsix.com>
2013-01-11 04:26:33 -0700

    Update Rails to 3.2.11 for security reasons

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-NullsixTagger::Application.config.secret_token = 'ae7b9a3a6c1af4fff871108f3186da1ec3046ee4e7266c624d809ef46cb169d46b353e96b019472153d3dc59d40a6946d34aac1a37c6f57e679f224724c999e9'
+NullsixTagger::Application.config.secret_token = '1b5d6da1fc6b49eae75b500fb90fcbb15b6d90249f6fe64cf4e84fe67193c5615ce0a6ae5771e1ae5ec00a31f54f58cc5403b3122370e82a4d65a8c951a9fbc2'
